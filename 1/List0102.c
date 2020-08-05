6210f979b26f8fad2db6c87934fe775b66cfd341
mtoshinori <45009334+mtoshinori@users.noreply.github.com>
2020-08-05 14:54:43 +0900

    Update List0102.c

--- 1/List0102.c
+++ 1/List0102.c
@@ -1,12 +1,12 @@
 /*サンプルプログラム int main(int argc, char const *argv[]) {
-	作成者：村尾　作成日：2018年8月5日*/
+	作成者：〇〇　作成日：2018年8月5日*/
 #include <stdio.h>
 
 // mainブロック
 int main(void)
 {
 	int age;	//年齢を入れる箱
-	age = 29;
+	age = 20;
 	printf("私は%d歳です\n",age);
 	return 0;
 }
